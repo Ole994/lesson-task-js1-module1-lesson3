f5a11a69a6a1b5ae88f56a429464dc7f92328706
Ole994 <69842859+Ole994@users.noreply.github.com>
2021-01-28 13:55:24 +0100

    2

--- js/product.js
+++ js/product.js
@@ -3,3 +3,28 @@ const product = {
     price: 99,
     onSale: true,
 };
+
+
+    console.log (product);
+} 
+
+
+
+
+
+
+
+// Lesson Task 3
+// The index.html file links to products.js. 
+//Loop through the array in products.js and display each object using HTML.
+// Provide a default value for the price property if it is missing.
+// If the product is sold out, display its name in red otherwise 
+//display it in green.
+// Change the properties 
+//in the objects in the array to test your code.
+// product.html links to product.js. Display 
+//each property of the object in product.js using HTML.
+// Provide a default value for the price
+// property if it is missing.
+// If the product is on sale, underline the name.
+// Change the properties in the object to test your code.
